3a789efdb5f77dc8eae4765f5ac49c74d709290d
Tim Ojo <ojotim@gmail.com>
2015-10-09 16:28:31 +0100

    implement is_quote_enclosed?

--- lib/csv_sniffer.rb
+++ lib/csv_sniffer.rb
@@ -1,4 +1,11 @@
 # This class contains functions to heuristically decipher certain information from a CSV file
 class CsvSniffer
 
+    # Reads the first line of the csv and returns true if the line starts and ends with " or '
+    def self.is_quote_enclosed?(filepath)
+      line = File.open(filepath, &:readline)
+      line.chomp!
+      return l line.start_with?('"') && line.end_with?('"') || line.start_with?("'") && line.end_with?("'")
+    end
+
 end
