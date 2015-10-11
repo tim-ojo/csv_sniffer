5bb44dc41644a9beb433341306a688d00555bb88
Tim Ojo <ojo.tim@gmail.com>
2015-10-10 22:06:10 -0400

    setup rdoc

--- csv_sniffer.gemspec
+++ csv_sniffer.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'csv_sniffer'
-  s.version     = '0.0.1'
-  s.date        = '2015-10-09'
+  s.version     = '0.0.2'
+  s.date        = '2015-10-10'
   s.summary     = "CSV library for heuristic detection of CSV properties"
   s.description = "CSV Sniffer is intended to provide utilities that will allow a user detect the delimiter character in use, whether the values in the CSV file are quote enclosed, whether the file contains a header, and more. The library is intended to detect information to be used as configuration inputs for CSV parsers."
   s.authors     = ["Tim Ojo"]
--- lib/csv_sniffer.rb
+++ lib/csv_sniffer.rb
@@ -2,11 +2,29 @@
 class CsvSniffer
 
     # Reads the first line of the csv and returns true if the line starts and ends with " or '
+    #
+    # Example:
+    #   CsvSniffer.is_quote_enclosed?("path/to/file")
+    #   =>  true
+    #
+    # Arguments:
+    #   filepath: (String)
+
     def self.is_quote_enclosed?(filepath)
       line = File.open(filepath, &:readline)
       line.chomp!.strip!
       return line.start_with?('"') && line.end_with?('"') || line.start_with?("'") && line.end_with?("'")
     end
+
+
+    # Gets the quote character in use in the file if one exists. Returns "'", """ or nil
+    #
+    # Example:
+    #   CsvSniffer.get_quote_char("path/to/file")
+    #   =>  "
+    #
+    # Arguments:
+    #   filepath: (String)
 
     def self.get_quote_char(filepath)
       if is_quote_enclosed?(filepath)
@@ -18,13 +36,23 @@ class CsvSniffer
       end
     end
 
-    # If the csv is quote enclosed then just get the delimiter after the first cell. Otherwise...
-    # Get the first line and count how many of the possible delimiters are present. If there is >1 of one of the
-    # delimiters and 0 of the others then, then we pick the max. If there are more than 0 of any of the others then
-    # we repeat the counting procedure for the next 50 lines until the condition is satisfied.
-    # If the condition is never satisfied then we simply pick the delimiter that occurs the most frequently, defaulting
-    # to the comma. Unless that delimeter's count is equal to the tab or pipe delimiter's count. In that case we return \t or |
+
+    # Heuristically detects the delimiter used in the CSV file and returns it
+    #
+    # Example:
+    #   CsvSniffer.detect_delimiter("path/to/file")
+    #   =>  "|"
+    #
+    # Arguments:
+    #   filepath: (String)
+
     def self.detect_delimiter (filepath)
+      # If the csv is quote enclosed then just get the delimiter after the first cell. Otherwise...
+      # Get the first line and count how many of the possible delimiters are present. If there is >1 of one of the
+      # delimiters and 0 of the others then, then we pick the max. If there are more than 0 of any of the others then
+      # we repeat the counting procedure for the next 50 lines until the condition is satisfied.
+      # If the condition is never satisfied then we simply pick the delimiter that occurs the most frequently, defaulting
+      # to the comma. Unless that delimeter's count is equal to the tab or pipe delimiter's count. In that case we return \t or |
 
       if is_quote_enclosed?(filepath)
         line = File.open(filepath, &:readline)
@@ -69,6 +97,7 @@ class CsvSniffer
       end
     end
 
+
     def self.max_delim_when_others_are_zero (line)
       freqOfPossibleDelims = get_freq_of_possible_delims(line)
 
@@ -90,6 +119,7 @@ class CsvSniffer
       end
     end
 
+
     def self.get_freq_of_possible_delims (line)
       freqOfPossibleDelims = Array.new(4) #=> [0 = ','] [1 = '\t'] [2 = ';'] [3 = '|']
       freqOfPossibleDelims[0] = line.count ","
@@ -99,6 +129,7 @@ class CsvSniffer
 
       return freqOfPossibleDelims
     end
+
 
     private_class_method :max_delim_when_others_are_zero
     private_class_method :get_freq_of_possible_delims
